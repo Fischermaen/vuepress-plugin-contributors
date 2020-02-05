298638ba15ec93a023a44dc9911b80b3f37750a5
PiPaPODerik <patrik.jakesch@gmail.com>
2020-02-05 16:00:02 +0100

    avatar style can now be used

--- components/contributors.vue
+++ components/contributors.vue
@@ -1,11 +1,11 @@
 <template>
-    <span class="contributors">
-        <ul>
-            <li v-for="(contributor, index) of $page.contributors">
-                <img :class="contributor.avatarClass" :src="contributor.avatarUrl" :height="$page.avatarSize"/><span class="name">{{ contributor.name }}</span><span :class="contributor.countClass">{{ contributor.count }}</span>
-            </li>
-        </ul>
-    </span>
+    <div class="contributors">
+        <div v-for="(contributor, index) of $page.contributors" :key="index" class="contributor">
+            <img :style="avatarSize" :class="[contributor.avatarClass, $page.avatarStyle]" :src="contributor.avatarUrl"/>
+            <span class="name">{{ contributor.name }}</span>
+            <span :class="contributor.countClass">{{ contributor.count }}</span>
+        </div>
+    </div>
 </template>
 
 <script>
